dd6676b055525f4a1e293159aac1477399e1bd9d
martadiazgarcia <mdiazg@hiberus.com>
2024-12-18 16:03:15 +0100

    ejemplo2

---
+++ ejemplo2.py
@@ -0,0 +1,20 @@
+def caracter(letra):
+    if letra=="a" or letra=="A":
+        print("La letra ", letra, " es una vocal")
+
+    elif letra=="e" or letra=="E":
+        print("La letra ", letra, " es una vocal")
+
+    elif letra=="i" or letra=="I":
+        print("La letra ", letra, " es una vocal")
+
+    elif letra=="o" or letra=="O":
+        print("La letra ", letra, " es una vocal")
+
+    elif letra=="u" or letra=="U":
+        print("La letra ", letra, " es una vocal")
+
+    else:
+        print("La letra ", letra, " es una consonante")
+
+caracter ("A")

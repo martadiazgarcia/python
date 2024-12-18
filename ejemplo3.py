512de6ebaaa47ffef5178fcfc64cf14ea5b404fd
martadiazgarcia <mdiazg@hiberus.com>
2024-12-18 16:21:08 +0100

    Update ejemplo3.py

--- ejemplo3.py
+++ ejemplo3.py
@@ -1,3 +1,5 @@
+#Implementar una función que reciba una lista y devuelva la suma de los valores de la lista
+
 def suma():
     miLista=[1, 2, 2, 3, 4]
     print(miLista[0]+miLista[1]+miLista[2]+miLista[3]+miLista[4])
